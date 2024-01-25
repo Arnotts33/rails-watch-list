b3489ab3125f6382d1fb5cf5490277d52f66bf69
Arnaud Lahaut <arnaudlht@gmail.com>
2024-01-25 17:02:14 +0100

    Last push of the day

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'bookmarks/new'
+  get 'bookmarks/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -6,5 +8,10 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
+  resources :movies, only: [:index, :show]
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
   # root "posts#index"
+  resources :bookmarks, only: [:destroy]
 end
